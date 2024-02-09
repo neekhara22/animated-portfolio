ec89ffdc05cd3a9eb8f81249d6276a66c3c9c832
Akash@22 <akash2202rits.com>
2024-02-09 18:51:01 +0530

    completed the homepage and the parallax and its animation

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,16 @@
+import "./services.scss"
+import {Motion} from "framer-motion"
+const Services = ()=>{
+    return <Motion.div className="services">
+    <Motion.div className="textContainer">
+    <p>I focus on helping your brand grow 
+    <br/>and move forward</p></Motion.div>
+    <Motion.div className="titleContainer">
+    <div className="title">
+    <img src="" alt=""/>
+    </div></Motion.div>
+    <Motion.div className="listContainer"></Motion.div>
+    </Motion.div>
+}
+
+export default Services
